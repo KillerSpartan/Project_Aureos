868d74386bcb4034fc8a05a28403bed2715df1ef
KillerSpartan <al1709130@upy.edu.mx>
2018-09-28 08:27:03 -0500

    This is the very first version of the practice

---
+++ Practice1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n_hello = 4;
+  while (n_hello <= 4) {
+    std::cout << "Hello Stranger!" << '\n';
+    n_hello = n_hello + 1;
+  }
+}
